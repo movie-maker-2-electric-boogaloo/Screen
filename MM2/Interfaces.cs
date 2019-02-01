4865d70d8d464c5c0e6bf2861ebeded59d5d8cb4
Wundero <sublimecraftftw@gmail.com>
2019-02-01 01:25:47 -0800

    Initial commit with some basic designing

---
+++ MM2/Interfaces.cs
@@ -0,0 +1,51 @@
+﻿using MM2.Time;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM2.Api
+{
+    interface IClip
+    {
+        Duration Duration();
+
+        IClip Subclip(Instant start, Duration length);
+
+        byte[] Read();
+    }
+
+    interface IProject
+    {
+        IList<ITimeline> Timelines();
+
+        void Save(File to);
+        void Load(File from);
+
+        string Name();
+    }
+
+    interface ITimeline : IEnumerable<IClip>
+    {
+        Duration Duration();
+
+        void Add(IClip clip); // adds to end
+        void Add(IClip clip, Instant time);
+
+        void Remove(IClip clip);
+    }
+
+    interface IPreviewer
+    {
+        void Preview(IProject project);
+    }
+
+    interface IRenderer
+    {
+        void Write(File to);
+
+        void Compose(IProject project);
+    }
+}
---
+++ MM2/Time.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Author: Wundero
+ * This class serves as time representations for the project in a more unified internal way.
+ * I will probably use time management libraries as a backend but for now this is fine.
+ */
+
+namespace MM2.Time
+{
+
+    interface Time
+    {
+        long Millis();
+        long Seconds();
+        long Minutes();
+        long Hours();
+    }
+
+    class Instant : Time
+    {
+        private long ms;
+
+        private Instant() { }
+        private Instant(long ms) => this.ms = ms;
+
+        public long Millis() => ms;
+
+        public long Seconds() => ms / 1000;
+
+        public long Minutes() => Seconds() / 60;
+
+        public long Hours() => Minutes() / 60;
+
+        public static Instant operator+ (Instant a, Instant b) => a.Add(b);
+
+        public Instant Add(Instant other) => new Instant(this.ms + other.ms);
+
+        public override bool Equals(object obj) => base.Equals(obj) || (obj is Instant && ((Instant) obj).ms == this.ms);
+
+        public override int GetHashCode() => (int) ms;
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    class Duration : Time
+    {
+
+        private Instant start;
+        private Instant end;
+        private long ms;
+
+        private Duration() { }
+
+        private Duration(Instant s, Instant e)
+        {
+            this.start = s;
+            this.end = e;
+            this.ms = e.Millis() - s.Millis();
+        }
+
+        public long Millis() => ms;
+
+        public long Seconds() => Millis() / 1000;
+
+        public long Minutes() => Seconds() / 60;
+
+        public long Hours() => Minutes() / 60;
+
+        public static Duration operator+ (Duration a, Duration b) => a.Add(b);
+
+        public Duration Add(Duration other) => new Duration(this.start, other.end.Add(this.end));
+
+        public override bool Equals(object obj) => base.Equals(obj) || (obj is Duration && ((Duration)obj).start.Equals(start) && ((Duration)obj).end.Equals(end));
+
+        public override int GetHashCode() => (int)Millis();
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
